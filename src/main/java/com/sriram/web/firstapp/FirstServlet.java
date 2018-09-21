11911f04fd83c2f04e3ae9103aad2a007ba29060
Guduri Sriram <shreeram.guduri@gmail.com>
2018-09-22 02:38:14 +0530

    FirstWebApp

---
+++ src/main/java/com/sriram/web/firstapp/FirstServlet.java
@@ -0,0 +1,44 @@
+package com.sriram.web.firstapp;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@Slf4j
+@WebServlet(
+        description = "First Servlet",
+        urlPatterns = {
+                "/FirstServlet",
+                "/FirstServlet.do"
+        },
+        initParams = {
+                @WebInitParam(name = "id", value = "1"),
+                @WebInitParam(name = "name", value = "Sriram")
+        }
+)
+public class FirstServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public static final String HTML_START = "<html><body>";
+    public static final String HTML_END = "</body></html>";
+
+    public FirstServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("inside  doGet of FirstServlet");
+        PrintWriter out = resp.getWriter();
+        Date date = new Date();
+        out.println(HTML_START + "<h2>Hi There!</h2><br/><h3>Date="+date +"</h3>"+HTML_END);
+    }
+}
