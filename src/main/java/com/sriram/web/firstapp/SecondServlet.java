9eb4a5d4af5288d98e9edabc2f45102a7d434369
Guduri Sriram <shreeram.guduri@gmail.com>
2018-09-22 14:55:02 +0530

    added SecondServlet

---
+++ src/main/java/com/sriram/web/firstapp/SecondServlet.java
@@ -0,0 +1,45 @@
+package com.sriram.web.firstapp;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.sriram.web.firstapp.FirstServlet.HTML_END;
+import static com.sriram.web.firstapp.FirstServlet.HTML_START;
+
+@Slf4j
+@WebServlet(
+        description = "Second Servlet",
+        urlPatterns = {
+            "/SecondServlet",
+            "/SecondServlet.do"
+        },
+        initParams = {
+            @WebInitParam(name = "name", value = "Sriram"),
+            @WebInitParam(name = "id", value = "2")
+        }
+)
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("Inside doGet method of SecondServlet");
+        PrintWriter out = resp.getWriter();
+        val headers = req.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String header = headers.nextElement();
+            val values = req.getHeaders(header);
+            while (values.hasMoreElements()) {
+                log.debug(header + " : " + values.nextElement());
+            }
+        }
+        out.println(HTML_START + "<h2>Hi There!</h2><br/><h3>Timestamp = " + System.currentTimeMillis() + "</h3>"+HTML_END);
+    }
+}
